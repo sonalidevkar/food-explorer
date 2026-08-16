39217bfae901e6cf4423d91c6341e1223e616602
unknown <sonalidevkar354@gmail.com>
2026-08-16 21:49:23 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Foods from "./pages/Foods";
 import FoodDetails from "./pages/FoodDetails";
 import Favorites from "./pages/Favorites";
