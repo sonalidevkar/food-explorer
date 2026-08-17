86a391f637cd77d701b170d23cdcc1fcc3b3042d
unknown <sonalidevkar354@gmail.com>
2026-08-17 21:32:28 +0530

    Fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
 import Foods from "./pages/Foods";
 import Favorites from "./pages/Favorites";
@@ -10,21 +8,12 @@ import Feedback from "./pages/Feedback";
 function App() {
   return (
     <BrowserRouter basename="/food-explorer">
-
-      <Navbar />
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/foods" element={<Foods />} />
-
         <Route path="/favorites" element={<Favorites />} />
-
         <Route path="/feedback" element={<Feedback />} />
-
       </Routes>
-
     </BrowserRouter>
   );
 }
